130fc47058b1d041f72c46efcbebfea4b825e7ab
axel <avillarmimbela@gmail.com>
2024-10-30 16:44:34 -0500

    add: creación y configuracion base

---
+++ src/main/java/backend/registro/equipos/Backend_re/BackendReApplication.java
@@ -0,0 +1,13 @@
+package backend.registro.equipos.Backend_re;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendReApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendReApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/registro/equipos/Backend_re/BackendReApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.registro.equipos.Backend_re;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendReApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
